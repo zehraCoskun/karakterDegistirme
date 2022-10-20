e2ebe3dedac4d90491a11bd0957cb3efa7396506
xzehrax <rabiazehraunal@hotmail.com>
2022-10-20 17:13:59 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Verilen string ifade içerisindeki ilk ve son karakterin yerini değiştirip tekrar ekrana yazdıran console uygulamasını yazınız.
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Cümle girişi yapınız");
+        string[] cumle = Console.ReadLine().Split(" ");
+        int kelimeSayisi = cumle.Count();
+        
+        foreach (var item in cumle)
+        {
+            char[] harfler = item.ToCharArray();
+            char tutucu = harfler[0];
+            harfler[0]=harfler[item.Length-1];
+            harfler[item.Length-1]=tutucu;
+            foreach (var harf in harfler)
+            {
+                Console.Write(harf);
+            }
+            Console.WriteLine(" ");
+
+
+        }
+    }
+}
